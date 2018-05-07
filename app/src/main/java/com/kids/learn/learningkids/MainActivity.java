0f78528a048fa636e81857480e9ac6504fca94c9
sudheer <sudheer1437@gmail.com>
2018-05-06 23:07:55 -0500

    Initial commit

---
+++ app/src/main/java/com/kids/learn/learningkids/MainActivity.java
@@ -0,0 +1,28 @@
+package com.kids.learn.learningkids;
+
+import android.app.ListActivity;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Adapter;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.Toast;
+
+public class MainActivity extends ListActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        String[] values = new String[] {"Fruits"};
+
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, R.layout.homescreenrowlayout, R.id.label, values);
+        setListAdapter(adapter);
+    }
+
+    @Override
+    protected void onListItemClick(ListView l, View v, int position, long id) {
+        String item = (String) getListAdapter().getItem(position);
+        Toast.makeText(this, item + " selected", Toast.LENGTH_LONG).show();
+    }
+}
